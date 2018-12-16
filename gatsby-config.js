a61ec0550f266a4bb1755ec171e8431e43af28f1
Enrique Balleste <eballeste@divisionof.com>
2018-12-15 22:53:19 -0500

    :ghost: adds initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Starter',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-plugin-root-import',
+      options: {
+        components: `${__dirname}/src/components`,
+        styles: `${__dirname}/src/styles`,
+      },
+    },
+  ],
+}
